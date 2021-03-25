bd28a01e77ed6efc0e480be04ea87de9f526564c
unknown <nandiniswaraj13@gmail.com>
2021-03-25 12:35:38 +0530

    [Nandini swaraj]Added Push operation in stack

---
+++ StackQueueUsingLinkedList/StackQueueUsingLinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StackQueueUsingLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackUsingLinkedList linkedListStack = new StackUsingLinkedList();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+           
+        }
+    }
+}
---
+++ StackQueueUsingLinkedList/StackQueueUsingLinkedList/StackUsingLinkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackQueueUsingLinkedList
+{
+    class StackUsingLinkedList
+    {
+        private Node top;
+        public StackUsingLinkedList()
+        {
+            this.top = null;
+        }
+
+        //30
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;//1300
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;//1400
+            }
+        }
+    }
+}
+
+      
